189b28e86767f4e856a6c51e8fa3feb694e6a43b
Hugo2203 <6286734@cegepmontpetit.ca>
2025-02-04 17:59:27 -0500

    Update Ex6 csv List et for.py

--- R03 Exercices Depart Hugo Bougie/Ex6 csv List et for.py
+++ R03 Exercices Depart Hugo Bougie/Ex6 csv List et for.py
@@ -17,7 +17,11 @@ import csv
 #  Des instructions détaillées sont données plus bas
 
 
-
+with open("csvs/Ex6 Competences.csv", 'r', encoding='utf-8') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter= '/')    
+    for line in csv_reader:
+    
+         print(line)
 
 
 
